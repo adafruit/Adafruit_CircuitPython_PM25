f8ce5793847217ca5f5f3cbf4d72bec7d41da6aa
David Glaude <dglaude@users.noreply.github.com>
2020-11-15 23:19:41 +0100

    Drop unnecessary temporary frame variable
    From @Gadgetoid PR https://github.com/pimoroni/Adafruit_CircuitPython_PM25/commit/eecc6b222ca838af2176b15c08140c03353c0d56

--- adafruit_pm25/__init__.py
+++ adafruit_pm25/__init__.py
@@ -96,7 +96,6 @@ class PM25:
             raise RuntimeError("Invalid PM2.5 checksum")
 
         # unpack data
-        frame = struct.unpack(">HHHHHHHHHHHH", self._buffer[4:28])
         (
             self.aqi_reading["pm10 standard"],
             self.aqi_reading["pm25 standard"],
@@ -110,6 +109,6 @@ class PM25:
             self.aqi_reading["particles 25um"],
             self.aqi_reading["particles 50um"],
             self.aqi_reading["particles 100um"],
-        ) = frame
+        ) = struct.unpack(">HHHHHHHHHHHH", self._buffer[4:28])
 
         return self.aqi_reading
